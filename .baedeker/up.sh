4ab4968287a69585abe676a0e19b092735fe6d85
Daniel Shiposha <mrshiposha@gmail.com>
2023-11-13 11:46:22 +0100

    feat: install baedeker and its lib in up.sh

--- .baedeker/up.sh
+++ .baedeker/up.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
+
+[[ -e baedeker ]] || (curl -L https://github.com/UniqueNetwork/baedeker/releases/download/v0.1.0-rc1/baedeker -o baedeker && chmod +x baedeker)
+[[ -e vendor/baedeker-library ]] || (mkdir -p vendor && git clone https://github.com/UniqueNetwork/baedeker-library vendor/baedeker-library)
+
 BDK_DIR=$(dirname $(readlink -f "$0"))
 RUST_LOG=info baedeker --spec=docker -J$BDK_DIR/vendor/ --generator=docker_compose=$BDK_DIR/.bdk-env --generator=docker_compose_discover=$BDK_DIR/.bdk-env/discover.env --secret=file=$BDK_DIR/.bdk-env/secret  --tla-str=relay_spec=rococo-local --input-modules='lib:baedeker-library/ops/nginx.libsonnet' --input-modules='lib:baedeker-library/ops/devtools.libsonnet' --tla-str=repoDir=$(realpath $BDK_DIR/..) $@ $BDK_DIR/rewrites.jsonnet
 cd $BDK_DIR/.bdk-env
